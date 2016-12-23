a3ed3e0cbaf8c21a158507da00718db1c301e7e4
91yun <admin@91yun.org>
2016-12-24 03:14:10 +0800

    修改了路由的测试点

--- test_91yun.sh
+++ test_91yun.sh
@@ -265,7 +265,8 @@ tracetest()
 	mtrgo "http://www.ipip.net/traceroute.php?as=1&a=get&n=1&id=100&ip=$IP" "上海电信（天翼云）"
 	mtrgo "http://www.ipip.net/traceroute.php?as=1&a=get&n=1&id=20&ip=$IP" "厦门电信CN2"
 	mtrgo "http://www.ipip.net/traceroute.php?as=1&a=get&n=1&id=12&ip=$IP" "重庆联通"
-	mtrgo "http://www.ipip.net/traceroute.php?as=1&a=get&n=1&id=2&ip=$IP" "天津移动"
+	mtrgo "http://www.ipip.net/traceroute.php?as=1&a=get&n=1&id=356&ip=$IP" "上海移动"
+	mtrgo "http://www.ipip.net/traceroute.php?as=1&a=get&n=1&id=160&ip=$IP" "北京教育网"
 }
 
 
@@ -276,7 +277,8 @@ backtracetest()
 	mtrback "101.227.255.45" "上海电信（天翼云）"
 	mtrback "117.28.254.129" "厦门电信CN2"
 	mtrback "113.207.32.65" "重庆联通"
-	mtrback "211.103.87.9" "天津移动"
+	mtrback "183.192.160.3" "上海移动"
+	mtrback "202.205.6.30" "北京教育网"
 }
 
 
