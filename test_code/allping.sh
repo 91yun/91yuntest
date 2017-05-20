873cf913745a172e2d688c9d011bf90a548cc331
91yun <rongdedee@sina.com>
2017-05-21 01:16:31 +0800

    no message

--- test_code/allping.sh
+++ test_code/allping.sh
@@ -13,12 +13,11 @@ ping_test(){
 	
 	#显示在显示器
 	next
-	echo -e $pingstrback | awk -F '^' '{printf("%-10s\t%-10s\t%-30s\t%-10s\t%-30s\t%-30s\t%-30s\n",$1,$2,$3,$4,$5,$6,$7)}' | tee -a $logfilename
 	
 	#记录进日志
 	echo "===开始进行全国PING测试===">>$logfilename
 	echo "===all ping start===" >> $logfilename
-	echo -e $pingstrback_all | awk -F '^' '{printf("%-3s\t%-30s\t%-15s\t%-20s\t%-3s\t%-7s\t%-7s\t%-7s\t%-3s\n",$1,$2,$3,$4,$5,$6,$7,$8,$9)}'>>$logfilename
+	echo -e $pingstrback_all | awk -F '^' '{printf("%-3s\t%-30s\t%-15s\t%-20s\t%-3s\t%-7s\t%-7s\t%-7s\t%-3s\n",$1,$2,$3,$4,$5,$6,$7,$8,$9)}' >> $logfilename
 	echo -e "===all ping end===\n\n" >> $logfilename
 	echo "===ping show===" >> $logfilename
 	echo -e $pingstrback | awk -F '^' '{printf("%-10s\t%-10s\t%-30s\t%-10s\t%-30s\t%-30s\t%-30s\n",$1,$2,$3,$4,$5,$6,$7)}'>>$logfilename
