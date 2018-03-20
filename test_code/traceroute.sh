9b27c3609b59be4b0d646fd6979b666b942ca401
林融 <linrong@linrongdeMacBook-Pro.local>
2018-03-20 19:41:50 +0800

    修改地址

--- test_code/traceroute.sh
+++ test_code/traceroute.sh
@@ -2,6 +2,6 @@
 
 traceroute()
 {
-	wget -q --no-check-certificate https://raw.githubusercontent.com/91yun/91yuntest/test/traceroute.py 
+	wget -q --no-check-certificate https://raw.githubusercontent.com/91yun/91yuntest/master/traceroute.py 
 	python -W ignore ${dir}/91yuntest/traceroute.py -l $logfilename -i $IP
 }
