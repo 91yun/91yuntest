64c9269ca1de536eb1bbf962e2d27ba0f9172445
91yun <admin@91yun.org>
2018-03-21 09:44:57 +0800

    画线

--- test_code/allping.sh
+++ test_code/allping.sh
@@ -1,5 +1,6 @@
 allping()
 {
+	next
 	wget -q --no-check-certificate https://raw.githubusercontent.com/91yun/91yuntest/master/test_code/allping.py 
 	python -W ignore ${dir}/91yuntest/allping.py -l ${dir}/$logfilename -i $IP
 }
--- test_code/traceroute.sh
+++ test_code/traceroute.sh
@@ -2,6 +2,7 @@
 
 traceroute()
 {
+	next
 	wget -q --no-check-certificate https://raw.githubusercontent.com/91yun/91yuntest/master/test_code/traceroute.py 
 	python -W ignore ${dir}/91yuntest/traceroute.py -l ${dir}/$logfilename -i $IP
 }
