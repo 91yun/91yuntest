7f18accd6d8ebe27ffab766658026ccf6d775af4
91yun <admin@91yun.org>
2018-08-25 00:02:17 +0800

    修bug

--- test_code/chinabw.sh
+++ test_code/chinabw.sh
@@ -1,4 +1,4 @@
-chinabw()
+cbw()
 {
 	#获得相关数据
 	wget --no-check-certificate https://raw.githubusercontent.com/91yun/speedtest-cli/master/speedtest_cli.py 1>/dev/null 2>&1
@@ -18,19 +18,23 @@ chinabw()
 	#写入日志文件
 	echo "$2|$upload|$download|$latency">>${dir}/$logfilename
 }
-next
-echo "===开始测试国内带宽===">>${dir}/$logfilename
-printf "%-18s%-18s%-20s%-12s\n" " Node Name" "Upload Speed" "Download Speed" "Latency"
-chinabw '3633' '上海 电信'
-chinabw '4741' '北京 电信'
-chinabw '7509' '杭州 电信'
-chinabw '4624' '成都 电信'
-chinabw '5083' '上海 联通'
-chinabw '4863' '西安 联通'
-chinabw '5726' '重庆 联通'
-chinabw '4665' '上海 移动'
-chinabw '4575' '成都 移动'
-chinabw '6168' '昆明 移动'
-chinabw '6611' '广东 移动'
-echo -e "===国内带宽测试结束==\n\n">>${dir}/$logfilename
+chinabw()
+{
+    next
+    echo "===开始测试国内带宽===">>${dir}/$logfilename
+    printf "%-18s%-18s%-20s%-12s\n" " Node Name" "Upload Speed" "Download Speed" "Latency"
+    cbw '3633' '上海 电信'
+    cbw '4741' '北京 电信'
+    cbw '7509' '杭州 电信'
+    cbw '4624' '成都 电信'
+    cbw '5083' '上海 联通'
+    cbw '4863' '西安 联通'
+    cbw '5726' '重庆 联通'
+    cbw '4665' '上海 移动'
+    cbw '4575' '成都 移动'
+    cbw '6168' '昆明 移动'
+    cbw '6611' '广东 移动'
+    echo -e "===国内带宽测试结束==\n\n">>${dir}/$logfilename
+}
+
 
