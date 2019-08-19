9fefb5d2da21291ad2e135a421adb376939140cf
91yun <admin@91yun.org>
2019-08-19 21:39:47 +0800

    Update gotoping.sh

--- test_code/gotoping.sh
+++ test_code/gotoping.sh
@@ -10,10 +10,10 @@ gotoping()
 {
 	echo "===开始测试跳板ping===">>${dir}/$logfilename
 	next
-	testping speedtest.tokyo.linode.com Linode日本
+	testping speedtest.tokyo2.linode.com Linode日本
 	testping hnd-jp-ping.vultr.com Vultr日本
 	testping 192.157.214.6 Budgetvm洛杉矶
-	testping downloadtest.kdatacenter.com kdatacenter韩国SK
+	testping speedtest.kdatacenter.com kdatacenter韩国SK
 	testping 210.92.18.1 星光韩国KT
 	echo "===跳板ping测试结束===">>${dir}/$logfilename
 }
